e61e5f9d8a51060803242686c4c83f464939aeb8
Decio Neto <deciom.neto10@gmail.com>
2022-02-13 22:12:43 -0300

    feat: add auth store flag

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -9,7 +9,9 @@
         <ul class="flex flex-row mt-1">
           <!-- Navigation Links -->
           <li>
-            <a class="px-2 text-white" href="#">Login / Register</a>
+            <a class="px-2 text-white" href="#" @click.prevent="toggleAuthModal">
+              Login / Register
+            </a>
           </li>
           <li>
             <a class="px-2 text-white" href="#">Manage</a>
@@ -19,3 +21,15 @@
     </nav>
   </header>
 </template>
+
+<script>
+
+export default {
+  name: 'Header',
+  methods: {
+    toggleAuthModal() {
+      this.$store.state.authModalShow = !this.$store.state.authModalShow;
+    },
+  },
+};
+</script>
