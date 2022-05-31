e2bcb8d93cf30778a6eac33ecf21fa98ef2c4a34
Decio Neto <deciom.neto10@gmail.com>
2022-05-31 00:56:21 -0300

    refactor: clean tailwind config file

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,12 +3,4 @@ module.exports = {
     './public/**/*.html',
     './src/**/*.{vue,js,ts,jsx,tsx}',
   ],
-  darkMode: 'media',
-  theme: {
-    extend: {},
-  },
-  variants: {
-    extend: {},
-  },
-  plugins: [],
 }
