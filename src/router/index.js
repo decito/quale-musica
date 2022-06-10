7e8d6d501eec9d9da1ddfb1f2e91937fcf41d913
Decio Neto <deciom.neto10@gmail.com>
2022-06-09 21:51:54 -0300

    feat(major): split home section to home component, add new routes with navigation guards, add manage page

--- src/router/index.js
+++ src/router/index.js
@@ -1,25 +1,51 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import Home from '../views/Home.vue'
+
+import Home from '@/views/Home.vue'
+import About from '@/views/About.vue'
+import Manage from '@/views/Manage.vue'
 
 const routes = [
   {
+    name: 'home',
     path: '/',
-    name: 'Home',
     component: Home,
   },
   {
+    name: 'about',
     path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue'),
+    component: About,
   },
-];
+  {
+    name: 'manage',
+    // alias: '/manage',
+    path: '/manage-music',
+    component: Manage,
+    beforeEnter: (to, from, next) => {
+      console.log('Manage Route Guard')
+
+      next()
+    },
+  },
+  {
+    path: '/manage',
+    redirect: { name: 'manage' },
+  },
+  {
+    path: '/:catchAll(.*)*',
+    redirect: { name: 'home' },
+  },
+]
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes,
+  linkExactActiveClass: 'text-yellow-500',
+})
+
+router.beforeEach((to, from, next) => {
+  console.log('Global Guard')
+
+  next()
 })
 
 export default router
