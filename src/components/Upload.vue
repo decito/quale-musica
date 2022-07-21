b11b9049195283ae342f464d0f4145cde1dae643
Decio Neto <deciom.neto10@gmail.com>
2022-07-21 15:05:30 -0300

    feat: add upload function integrated to firebase with fallback input, update fontawesome version

--- src/components/Upload.vue
+++ src/components/Upload.vue
@@ -1,14 +1,70 @@
 <script>
+import { auth, storage, songsCollection } from '@/includes/firebase'
+
 export default {
   name: 'Upload',
   data() {
     return {
       isDragover: false,
+      uploads: [],
     };
   },
   methods: {
-    upload() {
+    uploadFiles($event) {
       this.isDragover = false
+
+      const files = $event.dataTransfer
+        ? [...$event.dataTransfer.files]
+        : [...$event.target.files]
+
+      files.forEach((file) => {
+        if (file.type !== 'audio/mpeg') {
+          return
+        }
+
+        const storageRef = storage.ref()
+        const songRef = storageRef.child(`songs/${file.name}`)
+        const task = songRef.put(file)
+
+        const uploadIndex = this.uploads.push({
+          task,
+          currentProgress: 0,
+          name: file.name,
+          variant: 'bg-blue-400',
+          icon: 'fas fa-spinner fa-spin',
+          textClass: '',
+          uploadingState: true,
+        }) - 1
+
+        task.on('state_changed', (snapshot) => {
+          const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100
+          this.uploads[uploadIndex].currentProgress = progress
+        }, () => {
+          this.uploads[uploadIndex].variant = 'bg-red-400'
+          this.uploads[uploadIndex].icon = 'fas fa-xmark'
+          this.uploads[uploadIndex].textClass = 'text-red-400'
+          this.uploads[uploadIndex].uploadingState = false
+
+          // console.error(error)
+        }, async () => {
+          const song = {
+            uid: auth.currentUser.uid,
+            displayName: auth.currentUser.displayName,
+            originalName: task.snapshot.ref.name,
+            modifiedName: task.snapshot.ref.name,
+            genre: '',
+            commentCount: 0,
+          }
+
+          song.fileUrl = await task.snapshot.ref.getDownloadURL()
+          songsCollection.add(song)
+
+          this.uploads[uploadIndex].variant = 'bg-green-400'
+          this.uploads[uploadIndex].icon = 'fas fa-check'
+          this.uploads[uploadIndex].textClass = 'text-green-400'
+          this.uploads[uploadIndex].uploadingState = false
+        })
+      })
     },
   },
 }
@@ -19,8 +75,10 @@ export default {
   <div class="bg-white rounded border border-gray-200 relative flex flex-col">
     <div class="px-6 pt-6 pb-5 font-bold border-b border-gray-200">
       <span class="card-title">Upload</span>
+
       <i class="fas fa-upload float-right text-green-400 text-2xl"></i>
     </div>
+
     <div class="p-6">
       <!-- Upload Dropbox -->
       <div
@@ -34,30 +92,31 @@ export default {
         @dragover.prevent.stop="isDragover = true"
         @dragenter.prevent.stop="isDragover = true"
         @dragleave.prevent.stop="isDragover = false"
-        @drop.prevent.stop="upload"
+        @drop.prevent.stop="uploadFiles($event)"
       >
         <h5>Drop your files here</h5>
       </div>
+
+      <!-- eslint-disable-next-line vuejs-accessibility/form-control-has-label-->
+      <input type="file" multiple @change="uploadFiles($event)">
+
       <hr class="my-6" />
-      <!-- Progess Bars -->
-      <div class="mb-4">
-        <!-- File Name -->
-        <div class="font-bold text-sm">Just another song.mp3</div>
-        <div class="flex h-4 overflow-hidden bg-gray-200 rounded">
-          <!-- Inner Progress Bar -->
-          <div class="transition-all progress-bar bg-blue-400" style="width: 75%"></div>
+
+      <div
+        v-for="upload in uploads"
+        :key="upload.name"
+        class="mb-4"
+      >
+        <div class="font-bold text-sm" :class="upload.textClass">
+          <i :class="upload.icon"></i> {{ upload.name }}
         </div>
-      </div>
-      <div class="mb-4">
-        <div class="font-bold text-sm">Just another song.mp3</div>
+
         <div class="flex h-4 overflow-hidden bg-gray-200 rounded">
-          <div class="transition-all progress-bar bg-blue-400" style="width: 35%"></div>
-        </div>
-      </div>
-      <div class="mb-4">
-        <div class="font-bold text-sm">Just another song.mp3</div>
-        <div class="flex h-4 overflow-hidden bg-gray-200 rounded">
-          <div class="transition-all progress-bar bg-blue-400" style="width: 55%"></div>
+          <div
+          class="transition-all"
+          :class="[upload.variant, upload.uploadingState && 'progress-bar']"
+          :style="{ width: `${upload.currentProgress}%`}">
+          </div>
         </div>
       </div>
     </div>
