b14d7e33d28b4c0cb66f41fda78c9fa8f6b8aca6
Decio Neto <deciom.neto10@gmail.com>
2022-08-01 22:17:03 -0300

    feat: add song router

--- src/components/SongItem.vue
+++ src/components/SongItem.vue
@@ -16,9 +16,12 @@ export default {
     duration-300 hover:bg-gray-50"
   >
     <div>
-      <a href="#" class="font-bold block text-gray-600">
+      <router-link
+        :to="{ name: 'song', params: { id: song.docID } }"
+        class="font-bold block text-gray-600"
+      >
         {{ song.modifiedName }}
-      </a>
+      </router-link>
 
       <span class="text-gray-500 text-sm">
         {{ song.displayName }}
