d1a17322576d54ff0f53533c14fc67ae40841140
Decio Neto <deciom.neto10@gmail.com>
2022-08-25 21:12:49 -0300

    perf: add chunks to components page loading

--- src/router/index.js
+++ src/router/index.js
@@ -2,10 +2,10 @@ import { createRouter, createWebHistory } from "vue-router";
 
 import useUserStore from "@/stores/user";
 
-import Home from "@/views/Home.vue";
-import About from "@/views/About.vue";
-import Manage from "@/views/Manage.vue";
-import Song from "@/views/Song.vue";
+const Home = () => import("@/views/Home.vue");
+const About = () => import("@/views/About.vue");
+const Manage = () => import("@/views/Manage.vue");
+const Song = () => import("@/views/Song.vue");
 
 const routes = [
   {
