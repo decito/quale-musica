4650a51c2628a19c9437b1f2fe4d823da55be4e4
Decio Neto <deciom.neto10@gmail.com>
2022-09-11 00:06:40 -0300

    refactor: open music when click card instead of title only

--- src/components/SongItem.vue
+++ src/components/SongItem.vue
@@ -14,32 +14,32 @@ export default {
 <template>
   <li
     :id="`song-${song.docID}`"
-    class="flex justify-between items-center p-3 pl-6 cursor-pointer transition duration-300 hover:bg-gray-50 dark:hover:bg-gray-600"
+    class="cursor-pointer transition duration-300 hover:bg-gray-50 dark:hover:bg-gray-600"
   >
-    <div>
-      <router-link
-        :to="{ name: 'song', params: { id: song.docID } }"
-        class="font-bold block text-gray-700 dark:text-gray-100"
-      >
-        {{ song.modifiedName }}
-      </router-link>
+    <router-link
+      :to="{ name: 'song', params: { id: song.docID } }"
+      class="font-bold text-gray-700 dark:text-gray-100 flex justify-between items-center p-3 pl-6"
+    >
+      <div>
+        <p>{{ song.modifiedName }}</p>
 
-      <span class="text-gray-500 dark:text-gray-300 text-sm">
-        {{ song.displayName }}
-      </span>
-    </div>
-
-    <div class="text-gray-600 dark:text-gray-100 text-lg">
-      <router-link
-        v-slot="{ navigate }"
-        custom
-        :to="{ name: 'song', params: { id: song.docID }, hash: '#comments' }"
-      >
-        <span class="comments" @click="navigate">
-          <i class="fas fa-comments text-gray-600 dark:text-gray-100" />
-          {{ song.commentCount }}
+        <span class="text-gray-500 dark:text-gray-300 text-sm">
+          {{ song.displayName }}
         </span>
-      </router-link>
-    </div>
+      </div>
+
+      <div class="text-gray-600 dark:text-gray-100 text-lg">
+        <router-link
+          v-slot="{ navigate }"
+          custom
+          :to="{ name: 'song', params: { id: song.docID }, hash: '#comments' }"
+        >
+          <span class="comments" @click="navigate">
+            <i class="fas fa-comments text-gray-600 dark:text-gray-100" />
+            {{ song.commentCount }}
+          </span>
+        </router-link>
+      </div>
+    </router-link>
   </li>
 </template>
