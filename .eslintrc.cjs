147af8a8619668016455ee8dcc8fd470725ddd72
Décio Neto <deciom.neto10@gmail.com>
2022-11-15 12:55:16 -0300

    style: add prettierrc and editorconfig files

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,17 +1,17 @@
 /* eslint-env node */
-require("@rushstack/eslint-patch/modern-module-resolution");
+require("@rushstack/eslint-patch/modern-module-resolution")
 
 module.exports = {
   root: true,
   extends: [
     "plugin:vue/vue3-essential",
     "eslint:recommended",
-    "@vue/eslint-config-prettier",
+    "@vue/eslint-config-prettier"
   ],
   overrides: [
     {
       files: ["cypress/e2e/**.{cy,spec}.{js,ts,jsx,tsx}"],
-      extends: ["plugin:cypress/recommended"],
+      extends: ["plugin:cypress/recommended"]
     },
     {
       files: ["**.spec.js"],
@@ -21,14 +21,14 @@ module.exports = {
         expect: "readonly",
         test: "readonly",
         it: "readonly",
-        vi: "readonly",
-      },
-    },
+        vi: "readonly"
+      }
+    }
   ],
   env: {
-    node: true,
+    node: true
   },
   rules: {
-    "vue/multi-word-component-names": "off",
-  },
-};
+    "vue/multi-word-component-names": "off"
+  }
+}
