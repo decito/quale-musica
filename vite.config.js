e748b7b6881d92bb683214d18cd247809b9ad1f0
Décio Neto <deciom.neto10@gmail.com>
2025-05-06 01:13:58 -0300

    update all packages and config files

--- vite.config.js
+++ vite.config.js
@@ -1,39 +1,36 @@
-import { fileURLToPath, URL } from "node:url"
-
-import { defineConfig } from "vite"
-import { VitePWA } from "vite-plugin-pwa"
-import vue from "@vitejs/plugin-vue"
-// import { visualizer } from "rollup-plugin-visualizer";
+import vue from '@vitejs/plugin-vue'
+import { fileURLToPath, URL } from 'node:url'
+import { defineConfig } from 'vite'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     VitePWA({
-      registerType: "autoUpdate",
+      registerType: 'autoUpdate',
       devOptions: {
         enabled: true
       },
       manifest: {
-        name: "Qualé Musica",
-        theme_color: "#38bdf8",
+        name: 'Qualé Musica',
+        theme_color: '#38bdf8',
         icons: [
           {
-            src: "assets/img/pwa-192x192.png",
-            sizes: "192x192",
-            type: "image/png"
+            src: 'assets/img/pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
           }
         ]
       },
       workbox: {
-        globPatterns: ["**/*.{js,css,html,png,jpg}"]
+        globPatterns: ['**/*.{js,css,html,png,jpg}']
       }
     })
-    // visualizer({ open: true }),
   ],
   resolve: {
     alias: {
-      "@": fileURLToPath(new URL("./src", import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   },
   test: {
